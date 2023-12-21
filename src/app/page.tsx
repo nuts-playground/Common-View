06f519f5b4debca6b9613684788c5dc3f8405506
seokho <rkwhr3256@naver.com>
2023-12-22 01:17:56 +0900

    feat: 1기 유저 프로필 사진 업데이트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 import Image from 'next/image'
-import mainBanner from '../../public/main-banner.png'
 import Link from "next/link";
-import GihubIcon from "../../public/github-mark.png";
 import StartCard from "@/components/start-card";
-
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col items-center p-24">
+      <main className="flex min-h-screen flex-col items-center p-24 justify-center">
           <div className={`flex flex-wrap gap-3`}>
-              <StartCard name="아이언" link="withbyeongk" color="accent"/>
-              <StartCard name="호세" link="kiyoung92" color="accent"/>
-              <StartCard name="조니" link="devssk" color="accent"/>
-              <StartCard name="오스틴" link="simuseokyun" color="accent"/>
-              <StartCard name="에릭" link="seokho7" color="accent"/>
+              <StartCard userName="아이언" link="withbyeongk" color="accent" userImage={'/user-iron-profile.png'}/>
+              <StartCard userName="호세" link="kiyoung92" color="accent" userImage={'/user-jose-profile.png'}/>
+              <StartCard userName="조니" link="devssk" color="accent" userImage={'/user-jhonny-profile.png'}/>
+              <StartCard userName="오스틴" link="simuseokyun" color="accent" userImage={'/user-normal-profile.png'}/>
+              <StartCard userName="에릭" link="seokho7" color="accent" userImage={'/user-eric-profile.png'}/>
           </div>
       </main>
   )
--- src/components/start-card.tsx
+++ src/components/start-card.tsx
@@ -1,16 +1,22 @@
 import Link from "next/link";
 import React from "react";
 import Image from "next/image";
+// @ts-ignore
 import GihubIcon from "../../public/github-mark.png";
-import UserIcon from "../../public/user-image.png";
-const StartCard: React.FC<{ name: string, link: string, color: string }> = ({ name, link, color }) => {
+interface StartCardProps {
+    name: string;
+    link: string;
+    color: string;
+    userImage: string;
+}
+const StartCard: React.FC<{ userName: string, link: string, color: string, userImage: string }> = ({ userName, link, color, userImage }) => {
     return (
-        <div className="card w-96 bg-white text-neutral-content border-2">
+        <div className="card w-80 bg-white text-neutral-content border-2">
             <div className="card-body items-center text-center gap-5">
                 <h2 className="card-title text-gray-500 font-bold">
                     <div className={`flex gap-1 items-center`}>
-                        <Image className={`rounded-5`} src={UserIcon} width={25} height={25} alt={'깃허브 링크'}></Image>
-                        <p>{name} 사이트</p>
+                        <Image className={`rounded-5`} src={userImage} width={40} height={40} alt={'깃허브 링크'}></Image>
+                        <p>{userName} 사이트</p>
                     </div>
                 </h2>
                 <Link className={`text-gray-500`} href={`https://github.com/${link}`}>
